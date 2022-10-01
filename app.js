d13e83c27e60f7016964a01874b38a2f3f727397
Nelson Pedrosa Pena <xeobass@gmail.com>
2022-10-01 21:17:54 +0200

    arranque basico servidor node

---
+++ app.js
@@ -0,0 +1,22 @@
+const port = 9876;
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require('cors')
+
+const app = express();
+app.use(cors());
+
+// parse requests of content-type: application/json
+app.use(bodyParser.json({limit: '50mb', extended: true}));
+
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb',type:'application/x-www-form-urlencoded' }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Bienvenid@ a la aplicación de gestión de la protectora Colonias Canguesas." });
+});
+
+app.listen(port,()=>{
+    console.log(`Servidor escuchando en puerto ${port}`);
+});
